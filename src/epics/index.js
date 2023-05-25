88269ea10694f562f237aad5335661e8852d0e09
Heckfy <rr.akhmetoff@gmail.com>
2023-05-25 18:13:38 +0000

    Выполнил первую задачу

--- src/epics/index.js
+++ src/epics/index.js
@@ -7,6 +7,7 @@ import {
   filter,
   debounceTime,
   switchMap,
+  mergeMap,
   catchError
 } from 'rxjs/operators'
 import {
@@ -18,13 +19,13 @@ import {
   searchSkillsSuccess,
   searchSkillsFailure
 } from '../actions/actionCreators'
-import { of } from 'rxjs'
+import { iif, of, merge } from 'rxjs'
 
 export const changeSearchEpic = (action$) =>
   action$.pipe(
     ofType(CHANGE_SEARCH_FIELD),
     map((o) => o.payload.search.trim()),
-    filter((o) => o !== ''),
+    //filter((o) => o !== ''),
     debounceTime(100),
     map((o) => searchSkillsRequest(o))
   )
@@ -33,13 +34,15 @@ export const searchSkillsEpic = (action$) =>
   action$.pipe(
     ofType(SEARCH_SKILLS_REQUEST),
     map((o) => o.payload.search),
-    map((o) => new URLSearchParams({ q: o })),
+    map((o) => (o ? new URLSearchParams({ q: o }) : false)),
     tap((o) => console.log(o)),
     switchMap((o) =>
-      ajax.getJSON(`${process.env.REACT_APP_SEARCH_URL}?${o}`).pipe(
-        retry(3),
-        map((o) => searchSkillsSuccess(o)),
-        catchError((e) => of(searchSkillsFailure(e)))
-      )
+      o
+        ? ajax.getJSON(`${process.env.REACT_APP_SEARCH_URL}?${o}`).pipe(
+            retry(3),
+            map((o) => searchSkillsSuccess(o)),
+            catchError((e) => of(searchSkillsFailure(e)))
+          )
+        : of(searchSkillsSuccess([]))
     )
   )
